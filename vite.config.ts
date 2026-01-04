d77bab09290c15ffaff0e863de4348a7cf746972
3eleven3 <cameron.blair.wu+gh@gmail.com>
2026-01-04 22:55:14 +0000

    fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import treeShakeable from "rollup-plugin-tree-shakeable";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), treeShakeable()],
 	base: "/",
 	server: {
 		host: "0.0.0.0",
 		port: 5000,
 		allowedHosts: true,
+	},
+	build: {
+		outDir: "docs",
 	},
 });
